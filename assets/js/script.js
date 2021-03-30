cdda478bd73b5559af069e2648bf9d185c4c2a35
Graham Hall <ghall89@me.com>
2021-03-30 03:16:47 +0000

    copyright year will display date range starting in 2022

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,19 @@
-intro = document.querySelector('#intro');
+// intro = document.querySelector('#intro');
+// 
+// 
+// window.addEventListener('scroll', function() {
+// 	console.log(window.pageYOffset);
+// });
 
-
-window.addEventListener('scroll', function() {
-	console.log(window.pageYOffset);
-});
-
+// display copyright year 
 const copyrightYear = () => {
 	const year = new Date().getFullYear();
 	const yearSpan = document.querySelector('#current-year');
-	
-	yearSpan.textContent = year;
+	if(year > 2021) {
+		yearSpan.textContent = `2021 - ${year}`
+	} else {
+		yearSpan.textContent = year;
+	}
 };
 
 copyrightYear();
