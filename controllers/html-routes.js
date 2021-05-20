09d8c8c1953936e340ac2637900aacb59738569f
Graham Hall <ghall89@me.com>
2021-05-20 14:38:35 +0000

    set github username per portfolio piece

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -5,6 +5,7 @@ const data = [
 		"description": "A website to simulate managing a stock portfolio for a family, or small group. Built as a boot camp group project, my primary responsibility was bulding the front end using Handlebars and vanilla JavaScript.",
 		"img": "fam-fin.png",
 		"url": "https://familyfinancials.herokuapp.com/",
+		"githubUser": "htang2021",
 		"github": "famfin",
 		"tags": [
 			"js",
@@ -17,6 +18,7 @@ const data = [
 		"description": "A weather page created for the web development boot camp I attended. The goal of the project was to demonstrate my knowledge of CSS frameworks, REST APIs, and utilizing local storage to save and retrieve user data.",
 		"img": "weather.png",
 		"url": "https://ghall89.github.io/weather-dashboard/",
+		"githubUser": "ghall89",
 		"github": "weather-dashboard",
 		"tags": [
 			"api"
@@ -27,6 +29,7 @@ const data = [
 		"description": "My least favorite thing to do in CSS is to create drop shadows. I don't need to very often, but for the ocassions I do I wrote this quick web app to generate the code for me.",
 		"img": "css-shadow.png",
 		"url": "https://ghall89.github.io/css-shadow-tool/",
+		"githubUser": "ghall89",
 		"github": "css-shadow-tool",
 		"tags": [
 			"js"
@@ -37,6 +40,7 @@ const data = [
 		"description": "Created during my time attending a web development boot camp to demonstrate my knowlege of local browser storage, and manipulating the DOM.",
 		"img": "work-day.png",
 		"url": "https://ghall89.github.io/work-day-scheduler/",
+		"githubUser": "ghall89",
 		"github": "work-day-scheduler",
 		"tags": [
 			"js"
