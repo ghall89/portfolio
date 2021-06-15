7517426f885242a7a5d6a92b04da828a3cebb85a
Graham Hall <ghall89@me.com>
2021-06-15 21:23:48 +0000

    basic blog structure

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,26 +1,35 @@
 const router = require('express').Router();
-
-const { getData } = require('../db/db.js');
+const { getData, getBlog, getBlogPost } = require('../db/db.js');
 
 // homepage
 router.get('/', (req, res) => {
-	const data = getData('portfolio');
-	
 	res.render('home');
 });
 
 // portfolio
 router.get('/portfolio', (req, res) => {
-	const data = getData('portfolio');
+	const data = getData();
 	
 	res.render('portfolio', { data });
 });
 
 // contact
-router.get('/contact', (req, res) => {
-	const data = getData('portfolio');
-	
+router.get('/contact', (req, res) => {	
 	res.render('contact');
+});
+
+// blog
+router.get('/blog', (req, res) => {	
+	const data = getBlog();
+	res.render('blog', { data });
+});
+
+router.get('/blog/:id', (req, res) => {	
+	const data = getBlogPost(req.params.id);
+	if (!data.slug) {
+		res.redirect('/blog');
+	}
+	res.render('blog-post', { data });
 });
 
 module.exports = router;
